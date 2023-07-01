695e13191651f1b7eea5db2c69d61191dab8d2ec
EliseTomlin <edj46760@gmail.com>
2023-07-01 00:25:31 -0500

    debugging

--- public/main.js
+++ public/main.js
@@ -9,13 +9,15 @@ async function apiRequest(){
     const princessImage = document.getElementById('princessImage')
     princessImage.src = data.image
     princessImage.alt = 'Princess Image'
-        // document.getElementById('name').innerText = data.fullName
-        // document.getElementById('princessWorld').innerText = data.location
-        // document.getElementById('princessBirthPlace').innerText = data.birthPlace
-        // document.getElementById('princessFavActivity').innerText = data.favoriteActivity
-        // document.getElementById('princessFavFood').innerText = data.favoriteFood
-        // document.getElementById('alienImage').src = data.image
-        // document.getElementById('princessBestFriend').innerText = data.bestFriend
+        document.getElementById('name').innerText = data.fullName
+        document.getElementById('princessWorld').innerText = data.location
+        document.getElementById('princessBirthPlace').innerText = data.birthPlace
+        document.getElementById('princessFavActivity').innerText = data.favoriteActivity
+        document.getElementById('princessFavFood').innerText = data.favoriteFood
+        document.getElementById('princessImage').src = data.image
+        document.getElementById('princessBestFriend').innerText = data.bestFriend
+         
+
     }catch(error){
         console.log(error)
     }
