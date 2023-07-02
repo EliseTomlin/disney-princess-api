2abf8ca0375b794c44f39b80a098aaf8a0bffbf6
EliseTomlin <edj46760@gmail.com>
2023-07-02 00:53:30 -0500

    debugging image

--- server.js
+++ server.js
@@ -12,147 +12,146 @@ const connectionString = process.env.connectionString
 app.use(cors())
 app.use(express.json())
 
+const disneyPrincess = {
+    'elsa':{
+        'fullName': 'Queen Elsa of Arendelle',
+        'birthPlace': 'Arendelle',
+        'Location': 'Arendelle',
+        'favoriteFood': 'Chocolate ice cream',
+        'favoriteActivity': 'Ice skating and creating snow sculptures',
+        'bestFriend': 'Olaf the snowman',
+        'image': 'https://disney-princess-facts-api.onrender.com/elsa.jpg'
+    },
+    'anna': {
+        'fullName': 'Priness Anna of Arendelle',
+        'birthPlace': 'Arendelle',
+        'Location': 'Arendelle',
+        'favoriteFood': 'Chocolate cake',
+        'favoriteActivity': 'Exploring and spending time wtih her sister Elsa',
+        'bestFriend': 'Kristoff and Sven (and Elsa of course)'
+    },
 
-
-// const disneyPrincess = {
-//     'elsa':{
-//         'fullName': 'Queen Elsa of Arendelle',
-//         'birthPlace': 'Arendelle',
-//         'Location': 'Arendelle',
-//         'favoriteFood': 'Chocolate ice cream',
-//         'favoriteActivity': 'Ice skating and creating snow sculptures',
-//         'bestFriend': 'Olaf the snowman'
-//     },
-//     'anna': {
-//         'fullName': 'Priness Anna of Arendelle',
-//         'birthPlace': 'Arendelle',
-//         'Location': 'Arendelle',
-//         'favoriteFood': 'Chocolate cake',
-//         'favoriteActivity': 'Exploring and spending time wtih her sister Elsa',
-//         'bestFriend': 'Kristoff and Sven (and Elsa of course)'
-//     },
-
-//     'unknown': {
-//         'fullName': 'Unknown',
-//         'birthPlace': 'Unknown',
-//         'Location': 'Unknown',
-//         'favoriteFood': 'Unknown',
-//         'favoriteActivity': 'Unknown',
-//         'bestFriend': 'Unknown'
+    'unknown': {
+        'fullName': 'Unknown',
+        'birthPlace': 'Unknown',
+        'Location': 'Unknown',
+        'favoriteFood': 'Unknown',
+        'favoriteActivity': 'Unknown',
+        'bestFriend': 'Unknown'
     
-//     },
+    },
 
-//     'tiana': {
-//         'fullName': 'Tiana',
-//         'birthPlace': 'New Orleans, Louisiana',
-//         'Location': 'New Orleans, Louisiana',
-//         'favoriteFood': 'Beignets',
-//         'favoriteActivity': 'Cooking, singing, and dancing',
-//         'bestFriend': 'Louis the alligator and Ray the firefly'
+    'tiana': {
+        'fullName': 'Tiana',
+        'birthPlace': 'New Orleans, Louisiana',
+        'Location': 'New Orleans, Louisiana',
+        'favoriteFood': 'Beignets',
+        'favoriteActivity': 'Cooking, singing, and dancing',
+        'bestFriend': 'Louis the alligator and Ray the firefly'
     
-//     },
+    },
 
-//     'jasmine': {
-//         'fullName': 'Princess Jasmine',
-//         'birthPlace': 'Agrabah',
-//         'Location': 'Agrabah',
-//         'favoriteFood': 'Fresh fruit and Arabian cuisine',
-//         'favoriteActivity': 'Exploring the marketplace and flying on magic carpet',
-//         'bestFriend': 'Rajah and Genie'
+    'jasmine': {
+        'fullName': 'Princess Jasmine',
+        'birthPlace': 'Agrabah',
+        'Location': 'Agrabah',
+        'favoriteFood': 'Fresh fruit and Arabian cuisine',
+        'favoriteActivity': 'Exploring the marketplace and flying on magic carpet',
+        'bestFriend': 'Rajah and Genie'
     
-//     },
+    },
 
-//     'cinderella': {
-//         'fullName': 'cinderella',
-//         'birthPlace': 'A small kingdom',
-//         'Location': 'The prince castle',
-//         'favoriteFood': 'Pumpkin pie',
-//         'favoriteActivity': 'Singing, dancing, and dreaming',
-//         'bestFriend': 'Jaq and Gus, the mice, and her Fairy Godmother'
+    'cinderella': {
+        'fullName': 'cinderella',
+        'birthPlace': 'A small kingdom',
+        'Location': 'The prince castle',
+        'favoriteFood': 'Pumpkin pie',
+        'favoriteActivity': 'Singing, dancing, and dreaming',
+        'bestFriend': 'Jaq and Gus, the mice, and her Fairy Godmother'
     
-//     },
+    },
 
-//     'belle': {
-//         'fullName': 'Belle',
-//         'birthPlace': 'A small French village',
-//         'Location': 'The Beast castle',
-//         'favoriteFood': 'French pastries',
-//         'favoriteActivity': 'Reading books and going on adventures',
-//         'bestFriend': 'Lumiere the candlestick, Cogsworth, Mrs. Potts'
+    'belle': {
+        'fullName': 'Belle',
+        'birthPlace': 'A small French village',
+        'Location': 'The Beast castle',
+        'favoriteFood': 'French pastries',
+        'favoriteActivity': 'Reading books and going on adventures',
+        'bestFriend': 'Lumiere the candlestick, Cogsworth, Mrs. Potts'
     
-//     },
+    },
 
-//     'moana': {
-//         'fullName': 'Moana',
-//         'birthPlace': 'Motunui Island',
-//         'Location': 'The ocean and various islands',
-//         'favoriteFood': 'Coconut',
-//         'favoriteActivity': 'Sailing, wayfinding, and exploring the ocean',
-//         'bestFriend': 'Maui the demigod and Pua the pig'
+    'moana': {
+        'fullName': 'Moana',
+        'birthPlace': 'Motunui Island',
+        'Location': 'The ocean and various islands',
+        'favoriteFood': 'Coconut',
+        'favoriteActivity': 'Sailing, wayfinding, and exploring the ocean',
+        'bestFriend': 'Maui the demigod and Pua the pig'
     
-//     },
+    },
 
-//     'snow white': {
-//         'fullName': 'Snow White',
-//         'birthPlace': 'Unknown',
-//         'Location': 'The dwarfs cottage and the princes castle',
-//         'favoriteFood': 'Apple pie',
-//         'favoriteActivity': 'Singing, dancing, and spending tiem with animals',
-//         'bestFriend': 'The seven dwarfs, and the woodland animals'
+    'snow white': {
+        'fullName': 'Snow White',
+        'birthPlace': 'Unknown',
+        'Location': 'The dwarfs cottage and the princes castle',
+        'favoriteFood': 'Apple pie',
+        'favoriteActivity': 'Singing, dancing, and spending tiem with animals',
+        'bestFriend': 'The seven dwarfs, and the woodland animals'
     
-//     },
+    },
 
-//     'pocahontas': {
-//         'fullName': 'Pocahontas',
-//         'birthPlace': 'Virginia, United States',
-//         'Location': 'Jameston, Virginia',
-//         'favoriteFood': 'Cornbread',
-//         'favoriteActivity': 'Exploring nature, canoeing, and singing',
-//         'bestFriend': 'Meeko and Flint'
+    'pocahontas': {
+        'fullName': 'Pocahontas',
+        'birthPlace': 'Virginia, United States',
+        'Location': 'Jameston, Virginia',
+        'favoriteFood': 'Cornbread',
+        'favoriteActivity': 'Exploring nature, canoeing, and singing',
+        'bestFriend': 'Meeko and Flint'
     
-//     },
+    },
 
-//     'ariel': {
-//         'fullName': 'Princess Ariel',
-//         'birthPlace': 'Atlantica (underwater kingdom)',
-//         'Location': 'Atlantica',
-//         'favoriteFood': 'Fish and seafood',
-//         'favoriteActivity': 'Singing, exploring, and collecting human artifacts',
-//         'bestFriend': 'Flounder and Sebastian'
+    'ariel': {
+        'fullName': 'Princess Ariel',
+        'birthPlace': 'Atlantica (underwater kingdom)',
+        'Location': 'Atlantica',
+        'favoriteFood': 'Fish and seafood',
+        'favoriteActivity': 'Singing, exploring, and collecting human artifacts',
+        'bestFriend': 'Flounder and Sebastian'
     
-//     },
+    },
 
-//     'aurora': {
-//         'fullName': 'Princess Aurora',
-//         'birthPlace': 'Unknown',
-//         'Location': 'The castle in the Enchanted Forest',
-//         'favoriteFood': 'Berry tarts',
-//         'favoriteActivity': 'Dancing, singing, and spending time with nature',
-//         'bestFriend': 'Merryweather, Flora, and Fauna'
+    'aurora': {
+        'fullName': 'Princess Aurora',
+        'birthPlace': 'Unknown',
+        'Location': 'The castle in the Enchanted Forest',
+        'favoriteFood': 'Berry tarts',
+        'favoriteActivity': 'Dancing, singing, and spending time with nature',
+        'bestFriend': 'Merryweather, Flora, and Fauna'
     
-//     },
+    },
 
-//     'mulan': {
-//         'fullName': 'Fa Mulan',
-//         'birthPlace': 'China',
-//         'Location': 'Imperial City, China',
-//         'favoriteFood': 'Dumplings',
-//         'favoriteActivity': 'Martial Arts, archery, and horse riding',
-//         'bestFriend': 'Mushsu, Cri-Kee'
+    'mulan': {
+        'fullName': 'Fa Mulan',
+        'birthPlace': 'China',
+        'Location': 'Imperial City, China',
+        'favoriteFood': 'Dumplings',
+        'favoriteActivity': 'Martial Arts, archery, and horse riding',
+        'bestFriend': 'Mushsu, Cri-Kee'
     
-//     },
+    },
 
-//     'rapunzel': {
-//         'fullName': 'Priness Repunzel',
-//         'birthPlace': 'Kingdom of Corona',
-//         'Location': 'Kingdom of Corona',
-//         'favoriteFood': 'Hazelnut soup',
-//         'favoriteActivity': 'Painting, reading, and exploring the world outside her tower',
-//         'bestFriend': 'Unknown'
+    'rapunzel': {
+        'fullName': 'Priness Repunzel',
+        'birthPlace': 'Kingdom of Corona',
+        'Location': 'Kingdom of Corona',
+        'favoriteFood': 'Hazelnut soup',
+        'favoriteActivity': 'Painting, reading, and exploring the world outside her tower',
+        'bestFriend': 'Unknown'
     
-//     },
+    },
 
-// }
+}
 
 MongoClient.connect(connectionString,{useUnifiedTopology:true, useNewUrlParser:true})
     .then(client =>{
